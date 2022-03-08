726c4aa02036a665e3ab8c9239ae50c77aa79a4a
Omoefe Ugboma <Omoefe.Ugboma@investment-one.com>
2022-03-08 15:43:30 +0100

    Update product completed

--- src/app/components/product-form/product-form.component.ts
+++ src/app/components/product-form/product-form.component.ts
@@ -10,8 +10,11 @@ import { ApiService } from 'src/app/service/api.service';
 export class ProductFormComponent implements OnInit {
    
   productForm:any;
+  id = null;
 
   @Input() set product(val: any){
+    this.id = val.id;
+    // console.log(this.id);
     this.productForm = new FormGroup({
       title: new FormControl(val.title),
       price: new FormControl(val.price),
@@ -33,15 +36,27 @@ export class ProductFormComponent implements OnInit {
   }
 
   saveForm(){
-    console.log(this.productForm.value);
-    this.apiService.createProduct(
-      this.productForm.value.title,
-      this.productForm.value.price,
-      this.productForm.value.description, this.productForm.value.category
-    ).subscribe(
-      result => console.log(result),
-      error => console.log(error)
-    );
+    // console.log(this.productForm.value);
+    if(this.id){
+      this.apiService.updateProduct(this.id,
+        this.productForm.value.title,
+        this.productForm.value.price,
+        this.productForm.value.description, this.productForm.value.category
+      ).subscribe(
+        result => console.log(result),
+        error => console.log(error)
+      );
+    }else{
+      this.apiService.createProduct(
+        this.productForm.value.title,
+        this.productForm.value.price,
+        this.productForm.value.description, this.productForm.value.category
+      ).subscribe(
+        result => console.log(result),
+        error => console.log(error)
+      );
+    }
+    
   }
 
 }
