06c22f8a455d619de1d98ef5171b0558013da8b9
Omoefe Ugboma <Omoefe.Ugboma@investment-one.com>
2022-03-08 13:18:42 +0100

    Simple update

--- src/app/components/product-form/product-form.component.ts
+++ src/app/components/product-form/product-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-form',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-form.component.css']
 })
 export class ProductFormComponent implements OnInit {
-
+ 
+  @Input() product: any;
   constructor() { }
 
   ngOnInit(): void {
