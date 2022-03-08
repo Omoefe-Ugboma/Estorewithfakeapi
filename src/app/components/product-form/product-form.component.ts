3ed6736fd18f73bddb7a26e9400d5b1480a4cc84
Omoefe Ugboma <Omoefe.Ugboma@investment-one.com>
2022-03-08 15:17:52 +0100

    Completed post of products

--- src/app/components/product-form/product-form.component.ts
+++ src/app/components/product-form/product-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { ApiService } from 'src/app/service/api.service';
 
 @Component({
   selector: 'app-product-form',
@@ -7,26 +8,40 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./product-form.component.css']
 })
 export class ProductFormComponent implements OnInit {
- 
-  @Input() product: any;
+   
+  productForm:any;
 
-  productForm = new FormGroup({
-      title: new FormControl(''),
-      price: new FormControl(''),
-      description: new FormControl(''),
-      category: new FormControl(''),
-      image: new FormControl(''),
-      no_of_rating: new FormControl(''),
-      avg_rating: new FormControl('')
+  @Input() set product(val: any){
+    this.productForm = new FormGroup({
+      title: new FormControl(val.title),
+      price: new FormControl(val.price),
+      description: new FormControl(val.description),
+      category: new FormControl(val.category),
+      // image: new FormControl(val.image)
+      // no_of_rating: new FormControl(val.no_of_rating),
+      // avg_rating: new FormControl(val.avg_rating)
   });
+  }
 
-  constructor() { }
+
+
+  constructor(
+    private apiService:ApiService
+  ) { }
 
   ngOnInit(): void {
   }
 
   saveForm(){
     console.log(this.productForm.value);
+    this.apiService.createProduct(
+      this.productForm.value.title,
+      this.productForm.value.price,
+      this.productForm.value.description, this.productForm.value.category
+    ).subscribe(
+      result => console.log(result),
+      error => console.log(error)
+    );
   }
 
 }
